1671c18cd77dd52cbdb003ff06ffe7dafd43bfec
carm <carmelo.piccione@gmail.com>
2015-11-07 15:11:28 -0500

    formatting

--- src/result.ml
+++ src/result.ml
@@ -1,9 +1,9 @@
 module type S =
 sig
   type ('a, 'b) t
-  val ( >>= ) : ('a, 'd) t -> ('a -> ('b, 'd) t) -> ('b, 'd) t
+  val (>>=) : ('a, 'd) t -> ('a -> ('b, 'd) t) -> ('b, 'd) t
   val (>|=) : ('a, 'd) t -> ('a -> 'b) ->  ('b, 'd) t
-  val ( >>| ) : ('a, 'd) t -> ('a -> 'b) -> ('b, 'd) t
+  val (>>|) : ('a, 'd) t -> ('a -> 'b) -> ('b, 'd) t
   module Monad_infix :
   sig
     val (>>|) : ('a, 'd) t -> ('a -> 'b) -> ('b, 'd) t
@@ -16,7 +16,7 @@ sig
   val join : (('a, 'd) t, 'd) t -> ('a, 'd) t
   val ignore : ('a, 'd) t -> (unit, 'd) t
   val all : ('a, 'd) t list -> ('a list, 'd) t
-  val both : ('a, 'err) t -> ('b, 'err) t -> (('a * 'b), 'err) t
+  val both : ('a, 'd) t -> ('b, 'd) t -> (('a * 'b), 'd) t
   val all_ignore : (unit, 'd) t list -> (unit, 'd) t
   val map_error : ('ok, 'error1) t -> f:('error1 -> 'error2) -> ('ok, 'error2) t
 end
